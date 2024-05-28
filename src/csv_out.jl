e926bda5cbf658db9097c0214ac3e713744a4b40
Alexander Spears <alexander.spears@warwick.ac.uk>
2024-05-28 11:18:32 +0100

    Increase speed of job id comparison

--- src/csv_out.jl
+++ src/csv_out.jl
@@ -16,8 +16,8 @@ function update_csv_file!(output_filename::String, input_file::DataFrame, glob_p
     set_description(progress, "Processing files: ")
     for index in eachindex(vec(simulation_parameters["parameters"]))
         # Read job ids from results if possible to avoid reading duplicates.
-        job_ids = simulation_parameters["parameters"][index]["job_ids"]
-        to_read = findall(x -> split(x.name, "_")[end] in string.(job_ids), all_files)
+        job_ids = convert(Vector{Int64}, simulation_parameters["parameters"][index]["job_ids"])
+        to_read = findall(x -> parse(Int, split(x.name, "_")[end]) in job_ids, all_files)
         for file_index in to_read
             try
                 file_results = jldopen(all_files[file_index].path)["results"]
